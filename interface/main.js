a5328fe953506a04fe8426cdd2885c3cd9649e1f
Alice Jacka <jacka.alice@gmail.com>
2024-10-18 11:09:06 +1100

    Fix eslint issues

--- interface/main.js
+++ interface/main.js
@@ -1,8 +1,7 @@
-var counters = {};
-var configs = {};
-var defaultConfigs = {
+const counters = {};
+const defaultConfigs = {
   counters: {
-    'Phase': {
+    Phase: {
       position: 'top',
       phases: [
         { name: 'Hem', max: 30 },
@@ -12,41 +11,103 @@ var defaultConfigs = {
         { name: 'Foot', max: 60 },
         { name: 'Toe Decrease', max: 15 },
         { name: 'Toe Increase', max: 15 },
-        { name: 'Waste Yarn', max: 'Inf' },
-      ],
+        { name: 'Waste Yarn', max: Infinity }
+      ]
     },
-    'Total': {
+    Total: {
       position: 'bottom-left'
     },
-    'Colour': {
+    Colour: {
       position: 'bottom-right',
-      phases: [
-        { name: 'Red' },
-        { name: 'Green' },
-        { name: 'black', max: 2 }
-      ],
-      max: 3,
-    },
+      phases: [{ name: 'Red' }, { name: 'Green' }, { name: 'black', max: 2 }],
+      max: 3
+    }
   }
 };
 
-document.getElementById('increment').addEventListener('click', increment);
-document.addEventListener('keydown', (e) => {
-  if (e.key === ' ') {
-    e.preventDefault();
-    e.stopPropagation();
-    increment();
-  }
-});
-document.getElementById('reset').addEventListener('click', () => {
-  setup();
-});
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('config').value = JSON.stringify(defaultConfigs, null, 2);
-  setup();
-});
+const increment = () => {
+  for (const counterName in counters) {
+    if (Object.hasOwn(counters, counterName)) {
+      const counter = counters[counterName];
+      const valueElement = counter.elements.value;
+      const phaseElement = counter.elements.phase;
+      const maxElement = counter.elements.max;
 
-function setup() {
+      counter.state.value++;
+
+      if (!counter.state.max || counter.state.value <= counter.state.max) {
+        // No max, or haven't reached it yet
+        valueElement.textContent = counter.state.value;
+        continue;
+      }
+
+      counter.state.value = 1;
+
+      if (!counter.phases) {
+        valueElement.textContent = counter.state.value;
+        continue;
+      }
+
+      counter.state.phase++;
+
+      if (counter.state.phase >= counter.phases.length) {
+        counter.state.phase = 0;
+      }
+
+      counter.state.max =
+        counter.phases[counter.state.phase].max || counter.max || Infinity;
+      phaseElement.textContent = counter.phases[counter.state.phase].name;
+      maxElement.textContent = `/${counter.state.max}`;
+
+      valueElement.textContent = counter.state.value;
+    }
+  }
+};
+
+const addCounter = (screen, counterName, config) => {
+  counters[counterName] = config;
+  config.state = { value: 0 };
+  config.elements = {};
+
+  const mainElement = document.createElement('div');
+  mainElement.className = 'counter';
+  if (config.position) {
+    mainElement.classList.add(config.position);
+  }
+  config.elements.main = screen.appendChild(mainElement);
+
+  const nameElement = document.createElement('div');
+  nameElement.className = 'counter__name';
+  nameElement.textContent = counterName;
+  config.elements.name = mainElement.appendChild(nameElement);
+
+  if (config.phases) {
+    config.state.phase = 0;
+
+    const phaseElement = document.createElement('div');
+    phaseElement.className = 'counter__phase';
+    phaseElement.textContent = config.phases[0].name;
+    config.elements.phase = mainElement.appendChild(phaseElement);
+  }
+
+  const valueElement = document.createElement('div');
+  valueElement.className = 'counter__value';
+  valueElement.textContent = 0;
+  config.elements.value = mainElement.appendChild(valueElement);
+
+  // if (config.max) { inner += `<div class="counter__max">/${config.max}</div>` }
+  if (config.max || config.phases?.[0]?.max) {
+    config.state.max = config.phases?.[0].max || config.max || 'Inf';
+    const maxElement = document.createElement('div');
+    maxElement.className = 'counter__max';
+    maxElement.textContent = `/${config.state.max}`;
+    config.elements.max = mainElement.appendChild(maxElement);
+  }
+
+  return config;
+};
+
+const setup = () => {
   try {
     const configElement = document.getElementById('config');
     const config = JSON.parse(configElement.value);
@@ -55,83 +116,32 @@ function setup() {
     screen.innerHTML = '';
 
     for (const counterName in config.counters) {
-      if (Object.hasOwnProperty.call(config.counters, counterName)) {
+      if (Object.hasOwn(config.counters, counterName)) {
         addCounter(screen, counterName, config.counters[counterName]);
       }
     }
-  } catch (e) {
-    document.getElementById('error').innerText = e.message;
-    console.dir(e.message);
+  } catch (error) {
+    document.getElementById('error').innerText = error.message;
+    console.dir(error.message);
   }
-}
+};
 
-function addCounter(screen, counterName, config) {
-  counters[counterName] = config;
-  config.state = { value: 0 };
-  config.elements = {};
-
-  const mainElement = document.createElement('div');
-  mainElement.className = 'counter';
-  if (config.position) { mainElement.classList.add(config.position); }
-  config.elements['main'] = screen.appendChild(mainElement);
-
-  const nameElement = document.createElement('div');
-  nameElement.className = 'counter__name';
-  nameElement.textContent = counterName;
-  config.elements['name'] = mainElement.appendChild(nameElement);
-
-  if (config.phases) {
-    config.state.phase = 0;
-
-    const phaseElement = document.createElement('div');
-    phaseElement.className = 'counter__phase';
-    phaseElement.textContent = config.phases[0].name;
-    config.elements['phase'] = mainElement.appendChild(phaseElement);
+document.getElementById('increment').addEventListener('click', increment);
+document.addEventListener('keydown', (event) => {
+  if (event.key === ' ') {
+    event.preventDefault();
+    event.stopPropagation();
+    increment();
   }
-
-  const valueElement = document.createElement('div');
-  valueElement.className = 'counter__value';
-  valueElement.textContent = 0;
-  config.elements['value'] = mainElement.appendChild(valueElement);
-
-  // if (config.max) { inner += `<div class="counter__max">/${config.max}</div>` }
-  if (config.max || config.phases?.[0]?.max) {
-    config.state.max = config.phases?.[0].max || config.max || 'Inf';
-    const maxElement = document.createElement('div');
-    maxElement.className = 'counter__max';
-    maxElement.textContent = `/${config.state.max}`;
-    config.elements['max'] = mainElement.appendChild(maxElement);
-  }
-
-  return config;
-}
-
-function increment() {
-  for (const counterName in counters) {
-    if (Object.hasOwnProperty.call(counters, counterName)) {
-      const counter = counters[counterName];
-      const valueElement = counter.elements['value'];
-      const phaseElement = counter.elements['phase'];
-      const maxElement = counter.elements['max'];
-
-      counter.state.value++;
-
-      if (counter.state.max && counter.state.value > counter.state.max) {
-        counter.state.value = 1;
-
-        if (counter.phases) {
-          counter.state.phase++;
-          if (counter.state.phase >= counter.phases.length) {
-            counter.state.phase = 0;
-          }
-
-          counter.state.max = counter.phases[counter.state.phase].max || counter.max || 'Inf';
-          phaseElement.textContent = counter.phases[counter.state.phase].name;
-          maxElement.textContent = `/${counter.state.max}`;
-        }
-      }
-
-      valueElement.textContent = counter.state.value;
-    }
-  }
-}
+});
+document.getElementById('reset').addEventListener('click', () => {
+  setup();
+});
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('config').value = JSON.stringify(
+    defaultConfigs,
+    null,
+    2 // eslint-disable-line no-magic-numbers
+  );
+  setup();
+});
